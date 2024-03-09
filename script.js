111e1d51373a87b7df2390be003edad004be8de3
Manh <07mandao@skola.boras.se>
2024-03-09 16:17:11 +0100

    added my projects to the card

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-prikt
+const contactButton = document.getElementById("contact-btn");
+const dialog = document.querySelector("dialog");
+const closeDialog = document.getElementById("close-dialog");
+contactButton.onclick = function (){
+    dialog.show()
+}
+closeDialog.onclick = function (){
+    dialog.close();
+}
