692f4509347ba15a1da9ddfef49869971f469feb
Manh.D <07mandao@skola.boras.se>
2024-08-07 20:42:13 +0200

    importet a template

--- script.js
+++
@@ -1,9 +0,0 @@
-const contactButton = document.getElementById("contact-btn");
-const dialog = document.querySelector("dialog");
-const closeDialog = document.getElementById("close-dialog");
-contactButton.onclick = function (){
-    dialog.show()
-}
-closeDialog.onclick = function (){
-    dialog.close();
-}
